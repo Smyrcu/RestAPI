70d40645a5f93995940625ae198e54b3aef28d6b
Błażej Szadkowski <bl.szadkowski@gmail.com>
2022-05-15 22:10:04 +0200

    more and more

--- RestAPI/Services/DishService.cs
+++ RestAPI/Services/DishService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RestAPI.Entities;
 using RestAPI.Exceptions;
 using RestAPI.Models;
@@ -12,6 +13,8 @@ namespace RestAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        DishDto GetById(int restaurantId, int dishId);
+        List<DishDto> GetAll(int restaurantId);
     }
     
     public class DishService : IDishService
@@ -38,6 +41,36 @@ namespace RestAPI.Services
             _context.SaveChanges();
 
             return dishEntity.Id;
+        }
+
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
+            if (dish is null || dish.RestaurantId != restaurantId)
+                throw new NotFoundException("Dish not found");
+
+            var dishDto = _mapper.Map<DishDto>(dish);
+
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = _context
+                .Restaurants
+                .Include(r=> r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+
+            return dishDtos;
         }
     }
 }
